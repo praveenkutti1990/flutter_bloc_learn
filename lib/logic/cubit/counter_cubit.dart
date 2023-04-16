821fa3343803f753bf68ac4805d460d28247231f
praveenkutti1990 <praveenkutti1990@gmail.com>
2023-04-16 23:12:50 +0530

    added hydrated_bloc for counter cubit

--- lib/logic/cubit/counter_cubit.dart
+++ lib/logic/cubit/counter_cubit.dart
@@ -1,9 +1,12 @@
+import 'dart:convert';
+
 import 'package:bloc/bloc.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:meta/meta.dart';
 
 part 'counter_state.dart';
 
-class CounterCubit extends Cubit<CounterState> {
+class CounterCubit extends Cubit<CounterState> with HydratedMixin {
   CounterCubit() : super(CounterState(counterValue: 0));
 
   void increment() => emit(
@@ -11,4 +14,14 @@ class CounterCubit extends Cubit<CounterState> {
 
   void decrement() => emit(CounterState(
       counterValue: state.counterValue - 1, wasIncremented: false));
+
+  @override
+  CounterState? fromJson(Map<String, dynamic> json) {
+    return CounterState.fromMap(json);
+  }
+
+  @override
+  Map<String, dynamic>? toJson(CounterState state) {
+    return state.toMap();
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:path_provider/path_provider.dart';
+
 import 'package:flutter_bloc_learn/logic/cubit/connectivity_cubit.dart';
 import 'package:flutter_bloc_learn/logic/cubit/counter_cubit.dart';
 import 'package:flutter_bloc_learn/presentation/router/app_router.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  HydratedBloc.storage = await HydratedStorage.build(
+    storageDirectory: await getApplicationDocumentsDirectory(),
+  );
   runApp(MyApp());
 }
 
